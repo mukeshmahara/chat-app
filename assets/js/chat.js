6e2b22cda8229683fa92ef07b4a5ac0faa4992e0
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-03-03 19:08:47 +0545

    deployment port configurred

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,5 +1,5 @@
  //   making connection
- var socket = io.connect("http://localhost:4000");
+ var socket = io.connect("https://instant-mychat.herokuapp.com/");
 
  // Query DOM
  var message =  document.getElementById('message');
