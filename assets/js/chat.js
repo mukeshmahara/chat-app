822616b7ff86cd5fddae4bb3fa8f1a37f612506e
mukeshmahara <mukeshmahara1@gmail.com>
2022-11-16 22:36:30 +0545

    Socket initiated from localhost vai port 4000

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,5 +1,5 @@
  //   making connection
- var socket = io.connect("https://instant-mychat.herokuapp.com/");
+ var socket = io.connect("localhost:4000/");
 
  // Query DOM
  var message =  document.getElementById('message');
