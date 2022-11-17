92e919741b36bd86d36122b0d344a1025d341ead
mukeshmahara <mukeshmahara1@gmail.com>
2022-11-17 09:18:30 +0545

    Adding all username in the chat head

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,55 +1,49 @@
- //   making connection
- var socket = io.connect("localhost:4000/");
+//   making connection
+var socket = io.connect("localhost:4000/");
 
- // Query DOM
- var message =  document.getElementById('message');
- var handle = document.getElementById('handle');
- var output = document.getElementById('output');
- var btn =document.getElementById('send');
- var feedback = document.getElementById('feedback')
+// Query DOM
+var message = document.getElementById("message");
+var handle = document.getElementById("handle");
+var output = document.getElementById("output");
+var btn = document.getElementById("send");
+var feedback = document.getElementById("feedback");
 
 //  window.setInterval(function() {
 //   var scrollMsg = document.getElementById('output');
 //   scrollMsg.scrollTop = scrollMsg.scrollHeight;
 // }, 10);
 
-const chatMessage = document.querySelector('.chat-messages');
+const chatMessage = document.querySelector(".chat-messages");
 
- //Emit Event
- btn.addEventListener('click',()=>{
-   socket.emit('chat',{
-     handle:handle.value,
+//Emit Event
+btn.addEventListener("click", () => {
+  socket.emit("chat", {
+    handle: handle.value,
     // handle:'you',
-     message:message.value
-     
-   })
-   chatMessage.scrollTop = chatMessage.scrollHeight;
-   
- });
+    message: message.value,
+  });
+  chatMessage.scrollTop = chatMessage.scrollHeight;
+});
 
- message.addEventListener('keypress',()=>{
-   socket.emit('typing',handle.value);
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
+});
 
- })
+// Listen for event
+socket.on("chat", (data) => {
+  feedback.innerHTML = "";
 
+  output.innerHTML +=
+    "<p>" + "<strong>" + data.handle + ": </strong> " + data.message + "</p>";
+});
 
+socket.on("typing", (data) => {
+  feedback.innerHTML = "<p><em>" + data + " is typing message..." + "</em></p>";
+  topic.innerHTML = data +" is in chat"
+});
 
- // Listen for event
- socket.on('chat',(data)=>{
-   feedback.innerHTML = ""
- 
-   output.innerHTML +='<p>'+'<strong>'+data.handle+': </strong> '+data.message +'</p>'
+console.log(handle.value);
 
- })
+var termsAndConditions = document.getElementById("terms");
 
-
-
- socket.on('typing',(data)=>{
-  feedback.innerHTML = '<p><em>'+data+ ' is typing message...'+'</em></p>'
-})
-
-console.log(handle.value)
-
-var termsAndConditions = document.getElementById('terms');
-
-FormData.addEventListener('submit',(event)=>{})
+FormData.addEventListener("submit", (event) => {});
