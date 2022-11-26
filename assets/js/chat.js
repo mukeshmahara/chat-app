92f5a9a0b4cbe066c51d435f562f19e0f5703668
mukeshmahara <mukeshmahara1@gmail.com>
2022-11-26 10:35:23 +0545

    Fixes message alignment

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,6 +2,7 @@
 var socket = io.connect("localhost:4000/");
 
 // Query DOM
+var chatWindow = document.getElementById("chat-window")
 var message = document.getElementById("message");
 var handle = document.getElementById("handle");
 var output = document.getElementById("output");
@@ -24,7 +25,8 @@ message.addEventListener('keypress',(event)=>{
       message:message.value
       
     })
-    chatMessage.scrollTop = chatMessage.scrollHeight;
+    chatMessage.scrollTop = output.scrollTop + chatMessage.scrollHeight;
+    console.log(output.scrollTop)
     message.value = ""
   }
   
@@ -37,7 +39,7 @@ message.addEventListener('keypress',(event)=>{
     // handle:'you',
     message: message.value,
   });
-  chatMessage.scrollTop = chatMessage.scrollHeight;
+  chatMessage.scrollTop = chatMessage.scrollHeight
 });
 
 message.addEventListener("keypress", () => {
@@ -52,8 +54,17 @@ socket.on("chat", (data) => {
 
 function addMessage(data, user) {
   var element = document.createElement("div")
-  element.innerHTML =  data.handle[0] + data.message 
-  element.classList.add("rounded-pill", "card","mb-2",'p-2', "shadow-md")
+  var span = document.createElement("span")
+  span.append(element)
+  element.classList.add("col-md-6", "d-flex", "col-sm-6", "col-lg-6","mb-2",'p-3')
+  var div = document.createElement("div")
+  div.classList.add("text-dark", "bg-light","d-table","text-center","shadow-sm",  "border", "border-secondary", "rounded-circle" ,"col-1", 'p-3')
+  div.innerHTML =  data.handle[0]
+  let clonediv = div.cloneNode(true)
+  clonediv.innerHTML = data.message
+  clonediv.classList.remove("border", "rounded-circle")
+  element.append(div)
+  element.append(clonediv)
   output.append(element)
 }
 
